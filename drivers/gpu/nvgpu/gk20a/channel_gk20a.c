cc8031e45befd0f2f6ef2b5880f4390070dcd601
Terje Bergstrom <tbergstrom@nvidia.com>
2015-10-21 13:14:09 -0700

    gpu: nvgpu: Protect sync by an own lock
    Protect creation and deletion of sync by an own mutex. This prevents
    deadlock in channel abort when abort is called from submit path.
    
    Bug 200147887
    
    Change-Id: I5d6308b773c1d1a6a89d4590e2e74c74d691f79d
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/821127
    (cherry picked from commit 4be2cc60de49d22a87986c4122accf3427e94412)
    Reviewed-on: http://git-master/r/999489
    (cherry picked from commit 39f4d7ad2928768e0f530ef2018233ef20548953)
    Reviewed-on: http://git-master/r/1020290
    GVS: Gerrit_Virtual_Submit

--- drivers/gpu/nvgpu/gk20a/channel_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/channel_gk20a.c
@@ -323,10 +323,13 @@ void channel_gk20a_unbind(struct channel_gk20a *ch_gk20a)
 	 * resource at this point
 	 * if not, then it will be destroyed at channel_free()
 	 */
+	mutex_lock(&ch_gk20a->sync_lock);
 	if (ch_gk20a->sync && platform->aggressive_sync_destroy) {
+
 		ch_gk20a->sync->destroy(ch_gk20a->sync);
 		ch_gk20a->sync = NULL;
 	}
+	mutex_unlock(&ch_gk20a->sync_lock);
 }
 
 int channel_gk20a_alloc_inst(struct gk20a *g, struct channel_gk20a *ch)
@@ -389,10 +392,10 @@ void gk20a_channel_abort(struct channel_gk20a *ch, bool channel_preempt)
 		ch->g->ops.fifo.preempt_channel(ch->g, ch->hw_chid);
 
 	/* ensure no fences are pending */
-	mutex_lock(&ch->submit_lock);
+	mutex_lock(&ch->sync_lock);
 	if (ch->sync)
 		ch->sync->set_min_eq_max(ch->sync);
-	mutex_unlock(&ch->submit_lock);
+	mutex_unlock(&ch->sync_lock);
 
 	/* release all job semaphores (applies only to jobs that use
 	   semaphore synchronization) */
@@ -832,10 +835,12 @@ static void gk20a_free_channel(struct channel_gk20a *ch)
 	channel_gk20a_free_priv_cmdbuf(ch);
 
 	/* sync must be destroyed before releasing channel vm */
+	mutex_lock(&ch->sync_lock);
 	if (ch->sync) {
 		ch->sync->destroy(ch->sync);
 		ch->sync = NULL;
 	}
+	mutex_unlock(&ch->sync_lock);
 
 	/* release channel binding to the as_share */
 	if (ch_vm->as_share)
@@ -1624,11 +1629,13 @@ void gk20a_channel_update(struct channel_gk20a *c, int nr_completed)
 	 * the sync resource
 	 */
 	if (list_empty(&c->jobs)) {
+		mutex_lock(&c->sync_lock);
 		if (c->sync && platform->aggressive_sync_destroy &&
 			  gk20a_fence_is_expired(c->last_submit.post_fence)) {
 			c->sync->destroy(c->sync);
 			c->sync = NULL;
 		}
+		mutex_unlock(&c->sync_lock);
 	}
 	mutex_unlock(&c->jobs_lock);
 	mutex_unlock(&c->submit_lock);
@@ -1754,6 +1761,7 @@ int gk20a_submit_channel_gpfifo(struct channel_gk20a *c,
 
 	mutex_lock(&c->submit_lock);
 
+	mutex_lock(&c->sync_lock);
 	if (!c->sync) {
 		c->sync = gk20a_channel_sync_create(c);
 		if (!c->sync) {
@@ -1766,6 +1774,7 @@ int gk20a_submit_channel_gpfifo(struct channel_gk20a *c,
 		if (err)
 			return err;
 	}
+	mutex_unlock(&c->sync_lock);
 
 	/*
 	 * optionally insert syncpt wait in the beginning of gpfifo submission
@@ -1970,6 +1979,7 @@ int gk20a_init_channel_support(struct gk20a *g, u32 chid)
 	mutex_init(&c->ioctl_lock);
 	mutex_init(&c->jobs_lock);
 	mutex_init(&c->submit_lock);
+	mutex_init(&c->sync_lock);
 	INIT_LIST_HEAD(&c->jobs);
 #if defined(CONFIG_GK20A_CYCLE_STATS)
 	mutex_init(&c->cyclestate.cyclestate_buffer_mutex);

bdb1e4c127d0dbf1302bfd100daffdc7693b1aab
laufersteppenwolf <laufersteppenwolf@gmail.com>
2016-01-19 21:39:55 +0100

    shield: Tegra124 CPU and GPU OC
    Conflicts:
    	arch/arm/configs/cyanogenmod_shieldtablet_defconfig
    	build.sh

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -985,7 +985,7 @@ static int cpufreq_add_dev(struct device *dev, struct subsys_interface *sif)
 	cpumask_and(policy->cpus, policy->cpus, cpu_online_mask);
 
 	policy->user_policy.min = policy->min;
-	policy->user_policy.max = policy->max;
+	policy->user_policy.max = 2218500;
 
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 				     CPUFREQ_START, policy);

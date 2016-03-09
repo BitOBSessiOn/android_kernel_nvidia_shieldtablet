5bde85f3fdbbea716fed1e7cb72ead7f14cdeb3e
Mahantesh Kumbar <mkumbar@nvidia.com>
2016-03-09 12:30:51 +0530

    gpu: nvgpu: Enable ELPG when disabled due to reset
    Enable ELPG back whenever ELPG disable is done due to reset or recovery.
    Otherwise elpg_refcnt mismatch doesnt engage ELPG correctly
    
    Bug 200156347
    Bug 1716764
    
    Change-Id: I16dd47ebc647e631c1ace59099a36c92d4c3abb0
    Signed-off-by: Mahantesh Kumbar <mkumbar@nvidia.com>
    Reviewed-on: http://git-master/r/1027020
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Tested-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/gr_gk20a.h
+++ drivers/gpu/nvgpu/gk20a/gr_gk20a.h
@@ -1,7 +1,7 @@
 /*
  * GK20A Graphics Engine
  *
- * Copyright (c) 2011-2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -436,7 +436,10 @@ bool gk20a_gr_sm_debugger_attached(struct gk20a *g);
 		int err = 0; \
 		if (support_gk20a_pmu(g->dev)) \
 			err = gk20a_pmu_disable_elpg(g); \
-		if (err) return err; \
+		if (err) { \
+			gk20a_pmu_enable_elpg(g); \
+			return err; \
+		} \
 		err = func; \
 		if (support_gk20a_pmu(g->dev)) \
 			gk20a_pmu_enable_elpg(g); \

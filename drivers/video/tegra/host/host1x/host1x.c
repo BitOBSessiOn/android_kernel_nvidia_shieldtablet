0a87c9aca0090c5b90d6ef9f9b288f4252434d9a
Mikko Perttunen <mperttunen@nvidia.com>
2017-01-27 09:32:20 +0200

    video: tegra: host: Fix overflow issue allocation
    Change kmalloc to kmalloc_array to prevent overflow issues
    caused by large values supplied by user.
    
    Based on "video: tegra: host: Fix overflow issues in allocation"
    in nvhost/.
    
    Coverity ID 27942
    Bug 1856419
    Bug 1880704
    
    Change-Id: I5e96d0ec184543782dfe8814ad7e856b3b71221c
    Signed-off-by: Gagan Grover <ggrover@nvidia.com>
    Reviewed-on: http://git-master/r/1295053
    (cherry picked from commit 66adb8e35e0ad0d5ce383996fcc8bad3be8821f5)
    Reviewed-on: http://git-master/r/1312008
    (cherry picked from commit 9b414f949780a48a1c93d495465c8c39eb342df8)
    Reviewed-on: http://git-master/r/1324133
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Dhiren Parmar <dparmar@nvidia.com>

--- drivers/video/tegra/host/host1x/host1x.c
+++ drivers/video/tegra/host/host1x/host1x.c
@@ -3,7 +3,7 @@
  *
  * Tegra Graphics Host Driver Entrypoint
  *
- * Copyright (c) 2010-2015, NVIDIA Corporation. All rights reserved.
+ * Copyright (c) 2010-2017, NVIDIA Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
